2811567d8155bc97caa3ba59d9a3474995678bdd
Thriddle <dirk.veefkind@gmail.com>
2026-03-19 08:18:32 +0000

    fix: sluitende > ontbrak op article tag

--- web/app/themes/transactionservices/resources/views/components/dienst-card.blade.php
+++ web/app/themes/transactionservices/resources/views/components/dienst-card.blade.php
@@ -2,7 +2,7 @@
 
 @php(setup_postdata($post))
 
-<article x-data="{ show: false }" x-intersect.once="show = true" :class="show ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-6'" class="relative md:rounded-lg overflow-hidden group transition-all duration-700 ease-out"
+<article x-data="{ show: false }" x-intersect.once="show = true" :class="show ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-6'" class="relative md:rounded-lg overflow-hidden group min-h-[320px] transition-all duration-700 ease-out">
 
   @if(has_post_thumbnail($post))
   {!! wp_get_attachment_image(
@@ -16,10 +16,10 @@
         'sizes' => '(min-width:1024px) 33vw, 100vw'
       ]
   ) !!}
-  <div class="absolute inset-0 bg-red-500/50 group-hover:bg-black/40 transition"></div>
+  <div class="absolute inset-0 bg-black/20 group-hover:bg-black/40 transition"></div>
   @endif
 
-  <div class="relative z-10 p-6 flex flex-col justify-end min-h-[320px]">
+  <div class="relative z-10 p-6 flex flex-col justify-end h-full">
     <h2 class="text-2xl font-semibold mb-2 text-white">{{ get_the_title($post) }}</h2>
 
     @if(has_excerpt($post))
